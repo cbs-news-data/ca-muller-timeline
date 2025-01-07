eae0516e3f434d4e9050ff13b840e94a04377dc3
grace-manthey <45363240+grace-manthey@users.noreply.github.com>
2025-01-07 13:02:57 -0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,11 +6,15 @@ $(document).ready(function(){ // begin document.ready block
 	//jquery code here
 	$('#example').horizontalTimeline({
 		// useScrollBtns: false,
-		iconClass: {
-			"base": "fas fa-1x"
-			}
+		desktopDateIntervals: 300,
+	tabletDateIntervals: 250,
+	mobileDateIntervals: 200,
 	});
 
+	var height_of_container = $(".timeline-container").height();
+	console.log(height_of_container)
+
+	$(".timeline-container").css({"height":`${height_of_container + 200}`})
 
 	var pymChild = new pym.Child({ polling: 500 });
 	pymChild.sendHeight();
